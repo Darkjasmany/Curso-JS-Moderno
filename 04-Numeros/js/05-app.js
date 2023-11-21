04ab9ea592165d585575e58c88c0df3fadddc122
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2023-11-21 17:07:04 -0500

    Sección 4: Números en JavaScript - TERMINADO

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,15 @@
+// Veamos como ir incrementando un valor de 1 en 1 o en determinada cantidad
+
+// Incluir incremento por 1 y menos 1
+
+let puntaje = 5;
+puntaje++; //muestra y luego incrementa 5
+++puntaje; //incrementa y luego muestra 6
+
+puntaje--; //muestra y luego decrementa 5
+--puntaje; //descrementa y luego muestra 4
+
+puntaje += 3; //8
+puntaje -= 3; //2
+
+console.log(puntaje);

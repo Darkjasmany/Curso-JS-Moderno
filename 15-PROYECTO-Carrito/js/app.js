998d3c1efc1a76c9993789596da88370b4fa3291
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2023-12-06 11:28:42 -0500

    Sección 15: Proyecto - Creando un Carrito de Compras - Mostrando en el Carrito el curso seleccionado y Funcion para limpiar el HTML del carrito del tbody

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector("#carrito"); // Seleccionamos el carrito 
 const contenedorCarrito = document.querySelector("#lista-carrito tbody"); // Donde se van a ir agregando los cursos del carrito
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = []; // Inicia vacio este arreglo, es let porque vamos agregar y quitar elementos
 
 // Van a existir muchos Eventos por eso se recomienda crear una función para agruparlos
 
@@ -19,7 +20,7 @@ function agregarCurso(e) {
     if (e.target.classList.contains("agregar-carrito")) {
         //Para saber que curso estamos agregando
         const cursoSeleccionado =
-            e.target.parentElement.parentElement.parentElement;
+            e.target.parentElement.parentElement.parentElement; // Accedemos a todo ell DIV que tiene el contenido del curso
         leerDatosCurso(cursoSeleccionado);
         // console.log(e.target.parentElement.parentElement); //Subo dos veces de elemento
     }
@@ -29,7 +30,7 @@ function agregarCurso(e) {
 
 // Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso) {
-    console.log(curso); // recordar el curso tiene el HTML
+    // console.log(curso); // recordar el curso tiene el HTML
 
     // Crear un objeto con el contenido del curso actual
     const infoCurso = {
@@ -39,5 +40,50 @@ function leerDatosCurso(curso) {
         id: curso.querySelector("a").getAttribute("data-id"), // Selecciono el id del curso seleccionado
         cantidad: 1,
     };
-    console.log(infoCurso);
+    // console.log(infoCurso);
+
+    //Agrega elemetos al arreglo de carrito
+    //voy a tomar una copia de lo que hay en el carrito para no perder la informacion del carrito, va a ir creciendo de forma dinamica
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    // console.log(articulosCarrito);
+    carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+    // Limpiar el HTML
+    limpiarHTML();
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach((curso) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>
+          ${curso.imagen}
+          </td>
+          <td>
+          ${curso.titulo}
+          </td>
+          <td>
+          ${curso.precio}
+          </td>
+          <td>
+          ${curso.cantidad}
+          </td>
+        `;
+
+        // Agrega el HTML del carrito en el Tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Funcion para limpiar el HTML, elimina los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    // contenedorCarrito.innerHTML = "";
+
+    // Optimizar performan
+    // Si ese contenedor carrito tiene un elemento adentro este codigo se siguie ejecutando
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }

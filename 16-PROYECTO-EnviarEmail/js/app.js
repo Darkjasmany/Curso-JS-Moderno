947e5f94c9eebbacffdeb1d898c5cbec4734a05f
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2023-12-07 11:45:30 -0500

    Sección 16: Proyecto - Enviar Email - Seleccionar elementos de la interfaz

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,9 @@
+// Esto se ejecuta una vez que todo nuestro HTML se haya descargado
+document.addEventListener("DOMContentLoaded", function () {
+    // Variables
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector("#email");
+    const inputAsunto = document.querySelector("#asunto");
+    const inputMensaje = document.querySelector("#mensaje");
+    console.log(mensaje);
+});

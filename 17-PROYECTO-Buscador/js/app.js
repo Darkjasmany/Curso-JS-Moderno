c3591cdcc417295a132b163ab96f37aec09d1cc1
Jasmany Franco <jasmanyfranco@gmail.com>
2023-12-11 06:10:19 -0500

    Sección 17: Proyecto - Buscador de Autos - Generar el select de año

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,12 +1,17 @@
 // VARIABLES
 const resultado = document.querySelector("#resultado");
+const year = document.querySelector("#year");
+const yearMax = new Date().getFullYear(); // Año actual
+const yearMin = yearMax - 10;
 
-// EVENTOS
+// EVENTOS QUE SE EJECUTARAN CUANDO CARGUE TODO EL HTML
 document.addEventListener("DOMContentLoaded", () => {
-    mostrarAutos();
+    mostrarAutos(); // Muestra los automaviles al cargar todo el HTML
+
+    llenarSelectYear(); // LLena las opciones de Años
 });
 
-// FUNCTIONES
+// FUNCIONES
 function mostrarAutos() {
     // Iterar sobre el arreglo de autos que simula la base de datos
     autos.forEach((auto) => {
@@ -21,4 +26,16 @@ function mostrarAutos() {
         // Insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+// Genera los años del select
+function llenarSelectYear() {
+    // Este for corre del año actual hacia atras, me interesa que en el Select se visualice primero el año actual
+    for (let i = yearMax; i >= yearMin; i--) {
+        // console.log(i); // 2023 hasta el 2013
+        const opcion = document.createElement("option");
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de año al select
+    }
 }

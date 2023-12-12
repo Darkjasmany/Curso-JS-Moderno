3d1d1f05acce15bf23b8d08eb9d3b0c43e27fece
Jasmany Franco <jasmanyfranco@gmail.com>
2023-12-12 04:39:52 -0500

    Sección 17: Proyecto - Buscador de Autos - Leer el valor de los select de busqueda del formulario

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,14 +1,65 @@
 // VARIABLES
-const resultado = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+// Contenedor para los resultados
+const resultado = document.querySelector("#resultado");
+
+// Obtencion de años para filtros
 const yearMax = new Date().getFullYear(); // Año actual
 const yearMin = yearMax - 10;
+
+// Generar un objeto con la busqueda de acuerdo a todos los parametros de busqueda
+const datosBusqueda = {
+    marca: "",
+    year: "",
+    minimo: "",
+    maximo: "",
+    puertas: "",
+    transmision: "",
+    color: "",
+};
 
 // EVENTOS QUE SE EJECUTARAN CUANDO CARGUE TODO EL HTML
 document.addEventListener("DOMContentLoaded", () => {
     mostrarAutos(); // Muestra los automaviles al cargar todo el HTML
 
     llenarSelectYear(); // LLena las opciones de Años
+});
+
+// Event listener para los select de busqueda
+marca.addEventListener("change", (e) => {
+    // console.log(e.target.value); // Recibiendo el evento puedo ver el valor que se selecciono
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener("change", (e) => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener("change", (e) => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener("change", (e) => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener("change", (e) => {
+    datosBusqueda.puertas = e.target.value;
+});
+transmision.addEventListener("change", (e) => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener("change", (e) => {
+    datosBusqueda.color = e.target.value;
+    // console.log(datosBusqueda);
 });
 
 // FUNCIONES

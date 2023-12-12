65f360e88097be43a2f4c238d0a1d6620e934bce
Jasmany Franco <jasmanyfranco@gmail.com>
2023-12-12 06:37:57 -0500

    Sección 17: Proyecto - Buscador de Autos - Aplicando los filtros de busqueda de acuerdo a las seleciones de los diferentes select

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -27,7 +27,7 @@ const datosBusqueda = {
 
 // EVENTOS QUE SE EJECUTARAN CUANDO CARGUE TODO EL HTML
 document.addEventListener("DOMContentLoaded", () => {
-    mostrarAutos(); // Muestra los automaviles al cargar todo el HTML
+    mostrarAutos(autos); // Muestra los automaviles al cargar todo el HTML
 
     llenarSelectYear(); // LLena las opciones de Años
 });
@@ -36,34 +36,45 @@ document.addEventListener("DOMContentLoaded", () => {
 marca.addEventListener("change", (e) => {
     // console.log(e.target.value); // Recibiendo el evento puedo ver el valor que se selecciono
     datosBusqueda.marca = e.target.value;
+    filtrarAuto(); // Funcion que se llama para filtrar el contenido cada vez que exista una seleccion o cambio en el select
 });
 
 year.addEventListener("change", (e) => {
-    datosBusqueda.year = e.target.value;
+    datosBusqueda.year = parseInt(e.target.value); // Todo resultado que viene del formulario es un String, en este caso la fecha es un number por eso lo convertimos para poder hacer las comparaciones necesarias
+    // console.log(typeof datosBusqueda.year);
+    filtrarAuto();
 });
 
 minimo.addEventListener("change", (e) => {
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 });
 
 maximo.addEventListener("change", (e) => {
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
 });
 
 puertas.addEventListener("change", (e) => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
 });
 transmision.addEventListener("change", (e) => {
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 });
 
 color.addEventListener("change", (e) => {
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
     // console.log(datosBusqueda);
 });
 
 // FUNCIONES
-function mostrarAutos() {
+function mostrarAutos(autos) {
+    // Elimina el HTML previo
+    limpiarHTML();
+
     // Iterar sobre el arreglo de autos que simula la base de datos
     autos.forEach((auto) => {
         // Aplicamos Destrocturing para evitar definir los campos así ${auto.marca}
@@ -79,6 +90,14 @@ function mostrarAutos() {
     });
 }
 
+// Limpiar el HTML
+function limpiarHTML() {
+    //miestras haya algo
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
 // Genera los años del select
 function llenarSelectYear() {
     // Este for corre del año actual hacia atras, me interesa que en el Select se visualice primero el año actual
@@ -91,4 +110,95 @@ function llenarSelectYear() {
     }
 }
 
-// Prueba
+// Funcion que filtra en base a la busqueda
+function filtrarAuto() {
+    // console.log("Filtrando");
+    // Esto es una funcion de alto nivel, es una funcion que recibe como parametro otra funcion, antes el parametro de filter era un arrofunction que recibia el parametro de busqueda el arreglo autos que es el que simula nuestra bd
+
+    // Estos metodos soportar el changing encadenamiento de JS
+    const resultado = autos
+        .filter(filtrarMarca)
+        .filter(filtrarYear)
+        .filter(filtrarMinimo)
+        .filter(filtrarMaximo)
+        .filter(filtrarPuertas)
+        .filter(filtrarTransmision)
+        .filter(filtrarColor);
+
+    console.log(resultado);
+
+    mostrarAutos(resultado);
+}
+
+function filtrarMarca(auto) {
+    // console.log("marca");
+    // console.log(datosBusqueda); // aqui veo todos los autos con el filtrado de la marca que yo defini en el select
+    // hago destrocturing al arreglo datosBusqueda que contiene lo que filtr para seleccionar solo la marca
+    const { marca } = datosBusqueda;
+
+    // Si existe la marca seleccionada, cargo todos los autos de esa marca
+    if (marca) {
+        return auto.marca === marca;
+    }
+
+    // si no hay muestro todos los autos
+    return auto;
+}
+
+function filtrarYear(auto) {
+    const { year } = datosBusqueda;
+    // console.log(typeof year); // Este viene como String, antes de hacer la conversion
+    // console.log(typeof auto.year); // Este viene como Number
+    if (year) {
+        return auto.year === year;
+    }
+
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    if (minimo) {
+        return auto.precio >= minimo;
+    }
+
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    if (maximo) {
+        return auto.precio <= maximo;
+    }
+
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    const { puertas } = datosBusqueda;
+    // console.log(typeof puertas); // Este viene como String, antes de hacer la conversion
+    // console.log(typeof auto.puertas); // Este viene como Number
+
+    if (puertas) {
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    const { transmision } = datosBusqueda;
+    if (transmision) {
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto) {
+    const { color } = datosBusqueda;
+
+    if (color) {
+        return auto.color === color;
+    }
+
+    return auto;
+}

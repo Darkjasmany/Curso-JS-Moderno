a92dda52e6d8658fe0f03fe93e063419ef83d86e
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2024-01-16 17:59:10 -0500

    Sección 23: PROYECTO: Cotizador con Prototypes, mostrar total a pagar - POR TERMINAR

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -5,6 +5,48 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 }
 
+// Realiza la cotizacion con los datos
+Seguro.prototype.cotizarSeguro = function () {
+    // console.log(this.marca);
+    /*
+    1 = Americano 1.15
+    2 = Asiatico 1.05
+    3 = Europeo 1.35
+    */
+    let cantidad;
+    const base = 2000;
+
+    switch (this.marca) {
+        case "1":
+            cantidad = base * 1.15;
+            break;
+        case "2":
+            cantidad = base * 1.05;
+            break;
+        case "3":
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // Leer el año actual y traer la diferencia considerando el año seleccionado
+    const diferencia = new Date().getFullYear() - this.year;
+
+    // Cada año que la diferencia es mayr, el costo va a reducirse el 3%
+    cantidad -= (diferencia * 3 * cantidad) / 100;
+
+    // Si el seguro es basico se multiplica por un 30% más
+    // Si el seguro es basico se multiplica por un 50% más
+    if (this.tipo === "basico") {
+        cantidad *= 1.3;
+    } else {
+        cantidad *= 1.5;
+    }
+
+    return cantidad;
+};
+
 function UI() {}
 
 // Llena las opciones de los años
@@ -24,22 +66,48 @@ UI.prototype.llenarOpciones = () => {
 // Muestra alertas en pantalla
 // se usa el arroy function porque este prototype no hace referencia a this, ya que UI no recibe nada
 UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    limpiarAlerta();
     const div = document.createElement("div");
 
     if (tipo === "error") {
-        // div.classlist.add("error");
-        console.log("error");
+        div.classList.add("mensaje", "error");
     } else {
-        // div.classlist.add("correcto");
-        console.log("correcto");
+        div.classList.add("mensaje", "correcto");
     }
 
-    // div.classlist.add("mensaje", "mt-10");
+    div.classList.add("mensaje", "mt-10");
     div.textContent = mensaje;
 
     // Insertar en el HTML
     const formulario = document.querySelector("#cotizar-seguro");
     formulario.insertBefore(div, document.querySelector("#resultado")); // nuevo nodo, y el nodo de referencia donde quieres insertarlo
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+};
+
+function limpiarAlerta() {
+    const alerta = document.querySelector(".error");
+    // console.log(alerta);
+
+    if (alerta) {
+        alerta.remove();
+    }
+}
+
+UI.prototype.mostrarResultado = (total, seguro) => {
+    // Crear el resultaod
+    const div = document.createElement("div");
+    div.classList.add("mt-10");
+
+    div.innerHTML = `
+    <p class="header">Tu Resumen</p>
+    <p class="font-bold">Total: ${total}</p>
+    `;
+
+    const resultadoDiv = document.querySelector("#resultado");
+    resultadoDiv.appendChild(div);
 };
 
 // Instanciar UI
@@ -70,12 +138,19 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name= "tipo"]:checked').value;
     // console.log(tipo);
 
-    if (marca == "" || year == "" || tipo == "") {
-        console.log("No paso la validadcion");
+    if (marca === "" || year === "" || tipo === "") {
+        // console.log("No paso la validadcion");
         ui.mostrarMensaje("Todos los campos son obligatorios", "error");
-    } else {
-        console.log("Si paso la validadcion");
+        // ui.limpiarAlerta();
+        return;
     }
 
-    // console.log("cotizando");
+    ui.mostrarMensaje("Cotizando...", "exito");
+
+    // Instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo);
+    const total = seguro.cotizarSeguro();
+
+    // Utilizar el prototype que va a cotizar
+    ui.mostrarResultado(total, seguro);
 }

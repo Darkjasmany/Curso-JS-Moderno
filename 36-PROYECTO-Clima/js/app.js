78a36cbf5007b4dc1cdd67ad48e7745c5dd1f99f
Jasmany Franco <jasmanyfranco@gmail.com>
2024-02-20 03:26:12 -0500

    Sección 36: PROYECTO - API de Clima con Fetch API - TERMINADO

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -11,6 +11,143 @@ function buscarClima(e) {
     e.preventDefault();
 
     // Validar
+    const ciudad = document.querySelector("#ciudad").value;
+    const pais = document.querySelector("#pais").value;
 
-    // Consular API
+    if (ciudad === "" || pais === "") {
+        // Hubo un error
+        mostrarError("Ambos campos son Obligatorios");
+        return;
+    }
+
+    // Consultar API
+    consultarAPI(ciudad, pais);
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector(".error");
+
+    if (!alerta) {
+        const alerta = document.createElement("div");
+        alerta.classList.add(
+            "error", //  Esta clase la uso para validar que no se repita la alerta
+            "bg-red-100",
+            "border-red-400",
+            "text-red-700",
+            "px-4",
+            "py-3",
+            "rounded",
+            "max-w-md",
+            "mx-auto",
+            "mt-6",
+            "text-center",
+            "uppercase"
+        );
+        alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="block">${mensaje}</span>
+    `;
+        container.appendChild(alerta);
+
+        // Se elimine la alerta despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+}
+
+function consultarAPI(ciudad, pais) {
+    const appId = "dea793c4d063debd7559458a2acbf5e2";
+    // aqui obtuve de la documentacion de Openweather
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    spinner(); // Muestra un Spinner de carga
+
+    // console.log(url);
+    fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+            // console.log(datos);
+            limpiarHTML(); // Limpiar el HTML previo
+            if (datos.cod === "404") {
+                mostrarError("La Ciudad que estas consultando no exite");
+                return;
+            }
+
+            // Imprime la respuesta en el HTML
+            mostrarClima(datos);
+        });
+}
+
+function mostrarClima(datos) {
+    // console.log(resultado);
+    const {
+        name,
+        main: { temp, temp_max, temp_min },
+    } = datos;
+    // el resultados que da la API es en grados Kelvinm tenemos que convertir a grados centigrados
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement("p");
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add("font-bold", "text-2xl");
+
+    // Imprimir las temperaturas
+    const actual = document.createElement("p");
+    actual.innerHTML = `${centigrados} &#8451;`; // Esta es una entidad de html5 para imprimir el centigrado
+    actual.classList.add("font-bold", "text-6xl");
+
+    const tempMaxima = document.createElement("p");
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;
+    tempMaxima.classList.add("text-xl");
+
+    const tempMinima = document.createElement("p");
+    tempMinima.innerHTML = `Min: ${min} &#8451;`;
+    tempMinima.classList.add("text-xl");
+
+    const resultadoDiv = document.createElement("div");
+    resultadoDiv.classList.add("text-center", "text-white");
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+// function kelvinACentigrados(grados) {
+//     return parseInt(grados - 273.15); // redondea a entero
+//     // return (grados - 273.15).toFixed(2); // muestra dos decimales
+// }
+
+const kelvinACentigrados = (grados) => parseInt(grados - 273.15); // redondea a entero
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function spinner() {
+    limpiarHTML();
+
+    const divSpinner = document.createElement("div");
+    divSpinner.classList.add("sk-fading-circle");
+    divSpinner.innerHTML = `
+      <div class="sk-circle1 sk-circle"></div>
+      <div class="sk-circle2 sk-circle"></div>
+      <div class="sk-circle3 sk-circle"></div>
+      <div class="sk-circle4 sk-circle"></div>
+      <div class="sk-circle5 sk-circle"></div>
+      <div class="sk-circle6 sk-circle"></div>
+      <div class="sk-circle7 sk-circle"></div>
+      <div class="sk-circle8 sk-circle"></div>
+      <div class="sk-circle9 sk-circle"></div>
+      <div class="sk-circle10 sk-circle"></div>
+      <div class="sk-circle11 sk-circle"></div>
+      <div class="sk-circle12 sk-circle"></div>
+  `;
+    resultado.appendChild(divSpinner);
 }

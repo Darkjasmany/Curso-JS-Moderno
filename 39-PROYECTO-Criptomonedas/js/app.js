7f4b83b1683c4ed1d74ec09cd738bbe098c30f2f
Jasmany Franco <jasmanyfranco@gmail.com>
2024-04-11 07:14:19 -0500

    Sección 41: PROYECTO - Cotizador de Criptomonedas - Leer la moneda y criptomoneda seleccionada - POR TERMINAR

---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,38 @@
+const criptomonedasSelect = document.querySelector("#criptomonedas");
+
+// Crear un Promise que descarga todas las criptomonedas
+// El cual se va a ejecutar solamente si puede descargar todas las criptomonedas
+const obtenerCriptomonedas = (criptomonedas) =>
+    new Promise((resolve) => {
+        // En caso que sea correcto
+        resolve(criptomonedas);
+    });
+
+// Una ves que el docuento esta listo, vamos a cargar las criptomonedas
+document.addEventListener("DOMContentLoaded", () => {
+    consultarCriptomonedas();
+});
+
+function consultarCriptomonedas() {
+    const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+    fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((resultado) => obtenerCriptomonedas(resultado.Data))
+        .then((criptomonedas) => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach((criptomoneda) => {
+        // console.log(criptomoneda);
+        const { FullName, Name } = criptomoneda.CoinInfo;
+        // console.log(FullName);
+
+        const option = document.createElement("OPTION");
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+        // console.log(option);
+    });
+}

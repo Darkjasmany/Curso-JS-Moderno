66341ba5fb7d81fb2aeb94945591aa49e37511a9
Jasmany Franco <jasmanyfranco@gmail.com>
2024-05-09 06:43:07 -0500

    Sección 46: PROYECTO: CRM Crud con REST y Async Await - Eliminar Clientes de la API - POR TERMINAR

--- 44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,6 @@
 const url = "http://localhost:4000/clientes";
 
+// Cuando se crea nuevo cliente
 export const nuevoCliente = async (cliente) => {
     // console.log(cliente);
 
@@ -14,6 +15,17 @@ export const nuevoCliente = async (cliente) => {
 
         // Si se redirige es porque se inserto correctamente el cliente
         window.location.href = "index.html";
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Obtener todos los clientes
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
     } catch (error) {
         console.error(error);
     }

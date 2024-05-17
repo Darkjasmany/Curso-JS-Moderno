57c42badba01b995e0962bcf639d87057b22131e
Jasmany Franco <jasmanyfranco@gmail.com>
2024-05-17 07:36:06 -0500

    Sección 26: PROYECTO: Administrador de Citas de Pacientes - Actualizado 2024 - Agregando validación al formulario(Me adelante) - POR TERMINAR

--- 26-PROYECTO-AdministrarCitas-Actualizado2024/js/app.js
+++ 26-PROYECTO-AdministrarCitas-Actualizado2024/js/app.js
@@ -31,7 +31,48 @@ function datosCita(e) {
 }
 
 function submitCita(e) {
-    e.preventDefault();
-    console.log(citaObj);
-    console.log("Validando");
+    e.preventDefault(); // Para prevenir el evento de un metodo post o dirige a otra url
+
+    if (validar(citaObj)) {
+        // Mostrar un mensaje
+        // console.log("Todos los campos son obligatorios");
+        mostrarAlerta("Todos los campos son obligatorios");
+        return;
+    }
+}
+
+function validar(Obj) {
+    return !Object.values(Obj).every((input) => input !== "");
+}
+
+function mostrarAlerta(mensaje) {
+    const alerta = document.querySelector(".bg-red-100");
+
+    // Si no hay una alerta previa genera 1
+    if (!alerta) {
+        const alerta = document.createElement("P");
+        alerta.classList.add(
+            "bg-red-100",
+            "border-red-400",
+            "text-red-700",
+            "px-4",
+            "py-3",
+            "rounded",
+            "max-w-lg",
+            "mx-auto",
+            "mt-6",
+            "text-center"
+        );
+        alerta.innerHTML = `
+          <strong class="font-bold">Error!</strong>
+          <span class="block sm:inline">${mensaje}</span>
+        `;
+
+        const formulario = document.querySelector("#formulario-cita");
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
 }

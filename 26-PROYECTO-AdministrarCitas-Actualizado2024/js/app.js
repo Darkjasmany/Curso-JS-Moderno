3b9bdc3a022b0d27a76ca3d2b648a1b838b4077a
Jasmany Franco <jasmanyfranco@gmail.com>
2024-05-25 17:29:27 -0500

    Sección 26: PROYECTO: Administrador de Citas de Pacientes - Actualizado 2024 - Primeros pasos mostrando citas - POR TERMINAR

--- 26-PROYECTO-AdministrarCitas-Actualizado2024/js/app.js
+++ 26-PROYECTO-AdministrarCitas-Actualizado2024/js/app.js
@@ -25,54 +25,89 @@ const citaObj = {
     sintomas: "",
 };
 
+class Notificacion {
+    constructor({ texto, tipo }) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar();
+    }
+
+    mostrar() {
+        // Crear la notificacion
+        const alerta = document.createElement("DIV");
+        alerta.classList.add(
+            "alert",
+            "text-center",
+            "w-full",
+            "p-3",
+            "text-white",
+            "my-5",
+            "uppercase",
+            "font-blod",
+            "text-sm"
+        );
+
+        // Eliminar alertas duplicadas
+        const alertaPrevia = document.querySelector(".alert");
+        alertaPrevia?.remove(); // si existe una alerta la elimina, ? es un condicinal
+
+        // Si el tipo es error, agrega una clase
+        this.tipo === "error"
+            ? alerta.classList.add("bg-red-500")
+            : alerta.classList.add("bg-green-500");
+
+        // Mensaje de error
+        alerta.textContent = this.texto;
+
+        // Insertarla en el DOM
+        // parentElement, hace que vaya al elemento padre, indicamos que quiero introducir la alerta antes del formulario
+        formulario.parentElement.insertBefore(alerta, formulario);
+
+        // Quitar alerta despues de un tiempo
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+class AdminCitas {
+    constructor() {
+        this.citas = [];
+    }
+
+    agregar(cita) {
+        this.citas = [...this.citas, cita]; // con el expredoperator tomamos una copia de nuestro arreglo de citas y le mandamos la cita
+        console.log(this.citas);
+    }
+}
+
 // Funcion para mapear los value de los input en nuestro objeto de Cita
 function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
 }
 
+const citas = new AdminCitas();
+
 function submitCita(e) {
     e.preventDefault(); // Para prevenir el evento de un metodo post o dirige a otra url
 
+    // Para validar que todos los campos no esten vacios
+    // if (Object.values(citaObj).some(valor => valor.trim()==='')) {
+    //      console.log("Todos los campos son obligatorios");
+    // }
     if (validar(citaObj)) {
         // Mostrar un mensaje
-        // console.log("Todos los campos son obligatorios");
-        mostrarAlerta("Todos los campos son obligatorios");
+        new Notificacion({
+            texto: "Todos los campos son obligatorios",
+            tipo: "error",
+        });
         return;
     }
+
+    citas.agregar(citaObj);
 }
 
 function validar(Obj) {
     return !Object.values(Obj).every((input) => input !== "");
-}
-
-function mostrarAlerta(mensaje) {
-    const alerta = document.querySelector(".bg-red-100");
-
-    // Si no hay una alerta previa genera 1
-    if (!alerta) {
-        const alerta = document.createElement("P");
-        alerta.classList.add(
-            "bg-red-100",
-            "border-red-400",
-            "text-red-700",
-            "px-4",
-            "py-3",
-            "rounded",
-            "max-w-lg",
-            "mx-auto",
-            "mt-6",
-            "text-center"
-        );
-        alerta.innerHTML = `
-          <strong class="font-bold">Error!</strong>
-          <span class="block sm:inline">${mensaje}</span>
-        `;
-
-        const formulario = document.querySelector("#formulario-cita");
-        formulario.appendChild(alerta);
-
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
-    }
 }

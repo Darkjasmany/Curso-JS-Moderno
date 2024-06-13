6bc0ed52b5ce2ab67b90e7d809f1dc019e06600d
Jasmany Franco <jasmanyfranco@gmail.com>
2024-06-13 06:15:00 -0500

    Sección 53: PROYECTO: Instroducción a NodeJS - Creando un Proyecto c/ Express Sequelize Pug y Bootstrap - Creando un Header y Navegacion Principal - POR TERMINAR

--- 53-PROYECTO-AgenciaViajesNode/routes/index.js
+++ 53-PROYECTO-AgenciaViajesNode/routes/index.js
@@ -4,13 +4,15 @@ const router = express.Router(); // Estamos instanciando Express pero utilizando
 
 // req- lo que enviamos : res - lo que express responde
 router.get("/", (req, res) => {
-    res.send("Inicio");
+    res.render("inicio");
 });
 router.get("/nosotros", (req, res) => {
-    res.send("Nosotros");
+    // res.send("Nosotros"); // send imprime algo en pantalla
+
+    res.render("nosotros");
 });
 router.get("/contacto", (req, res) => {
-    res.send("Contacto");
+    res.render("contacto");
 });
 
 export default router;

c6e03b9a464a945a11acb1679de5f2dd56d5e1d9
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2024-06-24 17:09:46 -0500

    Sección 53: PROYECTO: Instroducción a NodeJS - Creando un Proyecto c/ Express Sequelize Pug y Bootstrap - DEPLOYMENT: Que son las variables de entorno? - POR TERMINAR

--- 53-PROYECTO-AgenciaViajesNode/controllers/paginasController.js
+++ 53-PROYECTO-AgenciaViajesNode/controllers/paginasController.js
@@ -4,11 +4,31 @@ import { Viaje } from "../models/Viajes.js";
 import { Testimonial } from "../models/Testimoniales.js";
 
 // req- lo que enviamos : res - lo que express responde
-const paginaInicio = (req, res) => {
-    res.render("inicio", {
-        pagina: "Inicio", //  pasamos la variable de pagina donde indicamos el titulo y lo vemos con h1
-        clase: "home",
-    });
+const paginaInicio = async (req, res) => {
+    //TODO: por lo que esta forma es la correcta y optima el codigo
+    // aqui cada promise se ejecuta al mismo tiempo y lo agregamos al arreglo deacuerdo a cada posicion del arreglo 0 a viajes 1 a testimonial
+    const promiseDB = [];
+
+    promiseDB.push(Viaje.findAll({ limit: 3 }));
+    promiseDB.push(Testimonial.findAll({ limit: 3 }));
+    try {
+        // TODO: Esto no es optimo xq puede que la 1er consulta demora mucho y no se ejecuta la segunda consulta
+        // Consultamos 3 viajes del modelo de viaje
+        // const viajes = await Viaje.findAll({ limit: 3 });
+        // const testimoniales = await Testimonial.findAll({ limit: 3 });
+
+        // TODO: pasamos las posiciones a la vista de acuerdo al resultado y con el await Promise.all arreancamos todas las consultas al mismo tiempo
+        const resultado = await Promise.all(promiseDB);
+
+        res.render("inicio", {
+            pagina: "Inicio", //  pasamos la variable de pagina donde indicamos el titulo y lo vemos con h1
+            clase: "home",
+            viajes: resultado[0],
+            testimoniales: resultado[1],
+        });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const paginaNosotros = (req, res) => {

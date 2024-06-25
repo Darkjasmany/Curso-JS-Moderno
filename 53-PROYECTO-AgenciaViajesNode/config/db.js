6a6d01e96094970d7dac1ec77f64af6f110aaa8b
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2024-06-25 17:08:02 -0500

    Sección 53: PROYECTO: Instroducción a NodeJS - Creando un Proyecto c/ Express Sequelize Pug y Bootstrap - DEPLOYMENT: Migrar infirmación de base de datos a Producción - POR TERMINAR

--- 53-PROYECTO-AgenciaViajesNode/config/db.js
+++ 53-PROYECTO-AgenciaViajesNode/config/db.js
@@ -1,21 +1,30 @@
 // TODO: Archivo de configuración del ORM Sequelize y conectar MySql
 
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config(); // de esta forma vamos a tener acceso a los valores de las variables de entorno
 
-const db = new Sequelize("agenciaviajes", "root", "sistemas1", {
-    host: "127.0.0.1",
-    port: "3306",
-    dialect: "mysql",
-    define: {
-        timestamps: false,
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-    operatorsAliases: false,
-});
+// console.log(process.env.DATABASE); // asi vamos acceder a los valores de nuestras variables de entorno
+
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: "3306",
+        dialect: "mysql",
+        define: {
+            timestamps: false,
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        operatorsAliases: false,
+    }
+);
 
 export default db;
